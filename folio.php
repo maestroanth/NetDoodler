39d97f9059dbdd47f80e91ee49429d3baf1698ed
maestroanth <maestroanth@gmail.com>
2016-12-02 23:20:45 -0700

    fixing weird permissions git is changing

--- folio.php
+++ folio.php
@@ -1,5 +1,5 @@
-<?php 
-	ob_start(); 
+<?php
+	ob_start();
 ?>
 
       <section class="large white content first">
@@ -14,33 +14,33 @@
 	             <div class="col-md-6" style="position: relative; margin-top: 5px;"> <!-- add another @media sub-class here to improve responsiveness -->
 	                 <div class="grid classic" style="position: relative; height: 500px; width: 75%; margin-left: 14%; margin-right: auto; margin-bottom: 40px; border: solid;">
 	          		  <div class="grid-sizer" "></div>
-						<a href="/images/mypic5.jpg" class="item old" target="_blank">
+						<a href="/images/mypic5.jpg" class="item old" target="_blank"> 
 			              <div class="bg" style="background-image: url(/images/mypic5.jpg); margin-bottom: 0px;" ></div>
-	
-			             
-			          
+
+
+
 	            	 </div></a>
 	             </div>
 
 	             <div class="col-md-6" style ="border: solid;">
                		 <h3 style="margin-top: 30px; font-weight: bold;">Anthony Walter</h3>
- 
+
   				<p style= "font-family: Maiandra GD, Movi Boli, Segoe Print;">  </p>
-  				
+
 
 				<p style= "font-family: Maiandra GD, Movi Boli, Segoe Print;"> Mr. Walter's Resumes: </p>
                 <div class="col-md-6" style="margin-top: -25px;">
                		 <p style="font-family: Maiandra GD;"><a href="/resumes/resumeProgrammingAnthonyWalter6-20-2016.pdf" download>Programming</a></p>
                 </div>
                 <div class="col-md-6" style="margin-top: -25px;">
- 
+
                		 <p style="font-family: Maiandra GD;"> <a href="/resumes/resumeMusic.pdf" download>Music/Pianist</a></p>
                 </div>
                 <div class="col-md-12 text-center">
                                 <p style= "font-family: Maiandra GD, Movi Boli, Segoe Print;"> Mr. Walter's Favorite Crayon Minion(s): </p>
                                 <h3 style= "color: red; margin-bottom: 60px;">The Red Minions</h3>
-                                  </div>     
-                 </div>               
+                                  </div>
+                 </div>
              </div>
 
             <div class="col-md-12 text-center" style="margin-top: 60px;">
@@ -57,15 +57,15 @@
                 </div>
                  <p style= "font-size: 1.2em;"><a style="font-family: quicksand, movi boli;" href="/Misc. Downloads/Cosimous.exe" download"><i>(Click for Playable Download and Extract the "Cosimous" Folder)</i></a></p>
                  <p><i>-"Now wouldn't expanding this game about a realm of different universes with varying laws of physics be awesome in a 3D VR/AR world; either for the Occulus or Google Glasses?"</i></p>
-			</div>  
-			
-				
+			</div>
+
+
 			<div class="col-md-12 text-center" style= "margin-top: 30px; border-top: solid; border-bottom: solid; line-height: 6em;">
-              
+
                 <p style= "font-family: Maiandra GD, Movi Boli, Segoe Print; margin-top: 40px; margin-bottom: 40px;"> Other Examples of Mr. Walter's Fine Work (USDA):  </p>
-           
- 			</div> 
-               
+
+ 			</div>
+
              <div class="col-md-12 text-center" style= "border-bottom: solid; padding-bottom: 40px;">
                 <div class="col-md-4" style= "margin-top: 50px;">
                 		<p>  <a href="http://www.idtools.org/id/grasshoppers/" target="_blank"> Grasshopper ID <img style="width: 90%;" src="images/grasshopper.jpg" alt="Smiley face" ></a></p>
@@ -76,25 +76,25 @@
                  <div class="col-md-4" style= "margin-top: 50px;">
                 		<p>  <a href="http://www.idtools.org/identify.php" target="_blank"> ITP Home<img style="width: 90%;" src="images/itphome.jpg" alt="Smiley face" ></a></p>
                 </div>
-                
 
 
 
-              </div> 
-				                
-				
+
+              </div>
+
+
 				<div class="col-md-12 text-center" style="margin-top: 120px; border-top: solid;">
-				
-				
-                <h2 style= "font-family: Maiandra GD, Tekton Pro, Movi Boli, Segoe Print; margin-top: 75px; margin-bottom: 60px;  text-decoration: underline;" 
+
+
+                <h2 style= "font-family: Maiandra GD, Tekton Pro, Movi Boli, Segoe Print; margin-top: 75px; margin-bottom: 60px;  text-decoration: underline;"
                 >Spot Holder for Our Next Doodler!!!</h2>
-                
-                
-                
+
+
+
                 <div style="margin-top: 40px;" class="col-md-12 text-center">
                   <div class="col-md-6">
                   <h3><b>Jane/John Doe</b></h3>
-                  
+
                                   <p style= "font-family: Maiandra GD, Movi Boli, Segoe Print; "> Mr./Ms. Doe's Curriculum Vitae: </p>
 
 
@@ -107,28 +107,28 @@
 	          		  <div class="grid-sizer" "></div>
 						<a href="/images/nextDoodler.png" class="item old" target="_blank" style="max-height: 400px;">
 			              <div class="bg" style="background-image: url(/images/nextDoodler.png); margin-bottom: 0px;" ></div>
-	
-			             
-			          
+
+
+
 	            	 </div></a>
-	          
+
                                 <p style= "font-family: Maiandra GD, Movi Boli, Segoe Print; margin-top: 10px; margin-bottom: 0px; "> Mr/Ms Doe's Favorite Crayon Minion(s): </p>
                                 <h3 style= "color: red; display: inline;">?</h3><h3 style= "color: blue; display: inline;">?</h3><h3 style= "color: green; display: inline; ">?</h3>
                                 <p></p>
-                                
- 
-                 </div>               
-             </div>
-             
-             
-              <div class="col-md-12 text-center" style= "margin-top: 30px;">
-                          
-                
 
-               </div> 
-               
+
+                 </div>
+             </div>
+
+
+              <div class="col-md-12 text-center" style= "margin-top: 30px;">
+
+
+
+               </div>
+
                 <div class="col-md-12 text-center" style= "margin-top: 80px; border-bottom: solid; padding-bottom: 80px;">
-               
+
 		               <p style= "font-family: Maiandra GD, Movi Boli, Segoe Print; border-top: solid; border-bottom: solid; line-height: 6em;"> Mr/Ms Doe's "Dream Tech Project": </p>
 		               <h3 style= "margin-top: 110px; margin-bottom: 50px;" >Maybe we can make your dream project come true?!</h3>
 					  <div class="col-md-12 text-center">
@@ -141,12 +141,12 @@
 		                <div class="col-md-4">
 						 <p  style= "width: 80%;"><img src="images/questionmark.gif" alt="Smiley face" ></p>
 		                </div>
-					</div> 
+					</div>
 		                <p><i>-"This is where you can elaborate on your dream project some more......"</i> </p>
-				</div> 
-				<!--  
+				</div>
+				<!--
 				 <div class="col-md-12 text-center" style= "margin-top: 30px;">
-				              
+
 		                <p style= "font-family: Maiandra GD, Movi Boli, Segoe Print;"> Other Examples of Mr/Ms Doe's Fine Work: </p>
 		                <div class="col-md-4">
 		                		<p>  <a target="_blank"> Can you do ASP.NET? </a><img style="width: 90%;" src="images/questionmark.gif" alt="Smiley face" ></p>
@@ -157,26 +157,26 @@
 		                 <div class="col-md-4">
 		                		<p>  <a target="_blank"> Any other skills? </a><img style="width: 90%;" src="images/questionmark.gif" alt="Smiley face" ></p>
 		                </div>
-	                
-	                   
-	                
-	             </div> 
+
+
+
+	             </div>
 	             -->
          <div class="col-md-12 text-center" style= "margin-top: 30px; border-bottom: solid; padding-bottom: 15px; ">
              	 <h1 id="ContactAFreeLancer" style="margin-top: 70px; margin-bottom: 95px; font-family: Maiandra GD;" ><a href="#" role="button" onclick="showContent('contact.php');">Contact A Doodler</a></h1>
-              </div> 
-             </div> 
+              </div>
+             </div>
 
 
-  
+
             </div>
            </div>
           </div>
 
 		</section>
 
- <?php 
- 		$output = ob_get_clean(); 
+ <?php
+ 		$output = ob_get_clean();
  		echo $output;
  		ob_end_flush();
  ?>
